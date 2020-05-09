f81330e5d48d0ed5469cca696c121418cf70df46
Daniel Leech <dan.t.leech@gmail.com>
2020-05-09 06:57:23 +0100

    DefinitionLocation extends Location

--- lib/DefinitionLocation.php
+++ lib/DefinitionLocation.php
@@ -2,32 +2,11 @@
 
 namespace Phpactor\ReferenceFinder;
 
-use Phpactor\TextDocument\ByteOffset;
 use Phpactor\TextDocument\Location;
-use Phpactor\TextDocument\TextDocumentUri;
 
 /**
  * @deprecated Is replaced by the Phpactor\\TextDocument\\Location value object
  */
-final class DefinitionLocation
+final class DefinitionLocation extends Location
 {
-    /**
-     * @var Location
-     */
-    private $location;
-
-    public function __construct(TextDocumentUri $uri, ByteOffset $byteOffset)
-    {
-        $this->location = new Location($uri, $byteOffset);
-    }
-
-    public function uri(): TextDocumentUri
-    {
-        return $this->location->uri();
-    }
-
-    public function offset(): ByteOffset
-    {
-        return $this->location->offset();
-    }
 }
