d6b9d119772954896c094e757810e3fc6426ec8d
Daniel Leech <dan.t.leech@gmail.com>
2020-05-24 11:30:28 +0100

    Add constants for search result types

--- lib/Search/NameSearchResult.php
+++ lib/Search/NameSearchResult.php
@@ -3,9 +3,13 @@
 namespace Phpactor\ReferenceFinder\Search;
 
 use Phpactor\Name\FullyQualifiedName;
+use RuntimeException;
 
 class NameSearchResult
 {
+    const TYPE_CLASS = 'class';
+    const TYPE_FUNCTION = 'function';
+
     /**
      * @var string
      */
@@ -18,6 +22,13 @@ class NameSearchResult
 
     private function __construct(string $type, FullyQualifiedName $name)
     {
+        $validTypes = [self::TYPE_FUNCTION, self::TYPE_CLASS];
+        if (!in_array($type, $validTypes)) {
+            throw new RuntimeException(sprintf(
+                'Name search result type "%s" is invalid, must be one of "%s"',
+                $type,implode('", "', $validTypes)
+            ));
+        }
         $this->type = $type;
         $this->name = $name;
     }
