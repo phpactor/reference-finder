252b4ff66f21be2c3d943e3e2e6b784855c9d592
Daniel Leech <dan.t.leech@gmail.com>
2021-01-02 10:58:48 +0000

    Add support for text document URI in name search result

--- lib/Search/NameSearchResult.php
+++ lib/Search/NameSearchResult.php
@@ -3,6 +3,7 @@
 namespace Phpactor\ReferenceFinder\Search;
 
 use Phpactor\Name\FullyQualifiedName;
+use Phpactor\TextDocument\TextDocumentUri;
 
 final class NameSearchResult
 {
@@ -16,21 +17,29 @@ final class NameSearchResult
      */
     private $name;
 
-    private function __construct(NameSearchResultType $type, FullyQualifiedName $name)
+    /**
+     * @var TextDocumentUri|null
+     */
+    private $uri;
+
+    private function __construct(NameSearchResultType $type, FullyQualifiedName $name, ?TextDocumentUri $uri = null)
     {
         $this->type = $type;
         $this->name = $name;
+        $this->uri = $uri;
     }
 
     /**
      * @param string|FullyQualifiedName $name
-     * @param string|NameSearchResultType $name
+     * @param string|NameSearchResultType $type
+     * @param string|TextDocumentUri $uri
      */
-    public static function create($type, $name): self
+    public static function create($type, $name, $uri = null): self
     {
         return new self(
             is_string($type) ? new NameSearchResultType($type) : $type,
-            is_string($name) ? FullyQualifiedName::fromString($name) : $name
+            is_string($name) ? FullyQualifiedName::fromString($name) : $name,
+            $uri ? (is_string($uri) ? TextDocumentUri::fromString($uri) : $uri) : null
         );
     }
 
@@ -42,5 +51,10 @@ final class NameSearchResult
     public function type(): NameSearchResultType
     {
         return $this->type;
+    }
+
+    public function uri(): ?TextDocumentUri
+    {
+        return $this->uri;
     }
 }
